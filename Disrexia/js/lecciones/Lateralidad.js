f6f25db24e33c4dc32c501ececa64f4f06c1bb1e
ARIIS29 <ari.sahulopez@gmail.com>
2023-05-01 18:46:23 -0600

    0.4.20 CAMBIOS DE IMAGEN

--- Disrexia/js/lecciones/Lateralidad.js
+++ Disrexia/js/lecciones/Lateralidad.js
@@ -1,11 +1,4 @@
-var audioCount = 0;
 function playSound(audio) {
-          var audio = new Audio('../audios/' + audio + '.mp3');
-          audio.play();
-          audio.addEventListener('ended', function() {
-              audioCount++;
-              if (audioCount == 4) {
-                  window.location.href = 'identifica letra.html';
-              }
-          });
-        }
+    var audio = new Audio('../audios/' + audio + '.mp3');
+    audio.play();
+}
