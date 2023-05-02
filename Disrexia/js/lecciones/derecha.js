aad537b5486043bb856be0382aae971fc2c9c1da
ARIIS29 <ari.sahulopez@gmail.com>
2023-05-02 10:34:12 -0600

    lecciones-lateralidad-B-D

---
+++ Disrexia/js/lecciones/derecha.js
@@ -0,0 +1,4 @@
+function playSound(audio) {
+    var audio = new Audio('../audios/' + audio + '.mp3');
+    audio.play();
+}
---
+++ Disrexia/js/lecciones/letraD.js
@@ -0,0 +1,4 @@
+function playSound(audio) {
+    var audio = new Audio('../audios/' + audio + '.m4a');
+    audio.play();
+}
