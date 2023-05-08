4a753844cfbca756a6f2a004441896110f1b85f9
marco-29 <marcoantonioreyesperez1@gmail.com>
2023-05-08 01:43:51 -0600

    0.5.7 leones

---
+++ Disrexia/js/evaluacion/leones.js
@@ -0,0 +1,35 @@
+function cambiar_fondo_b(x) {
+    if(x.style.background=="rgb(6, 155, 1)") {
+        x.style.background="#00000071";
+    }else {
+        x.style.background="#069B01";
+    }
+    return false;
+}
+
+function cambiar_fondo_d(x) {
+    if(x.style.background=="rgb(1, 136, 155)") {
+        x.style.background="#00000071";
+    }else {
+        x.style.background="#01889B";
+    }
+    return false;
+}
+
+function cambiar_fondo_p(x) {
+    if(x.style.background=="rgb(243, 21, 21)") {
+        x.style.background="#00000071";
+    }else {
+        x.style.background="#F31515";
+    }
+    return false;
+}
+
+function cambiar_fondo_q(x) {
+    if(x.style.background=="rgb(216, 21, 243)") {
+        x.style.background="#00000071";
+    }else {
+        x.style.background="#D815F3";
+    }
+    return false;
+}
